beadb8a3fa52c8076fcdb6b6ec3d57c1f19f8db7
M26I <marija.ivanisevic267@gmail.com>
2025-03-18 14:37:39 +0000

    model improving

--- src/train.py
+++ src/train.py
@@ -5,23 +5,44 @@ import seaborn as sns
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_absolute_error, mean_squared_error
+from sklearn.preprocessing import StandardScaler
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import GridSearchCV
+import xgboost as xgb
+xgb_model = xgb.XGBRegressor()
+
+
+
 
 df = pd.read_csv(r"C:\Users\marij\Desktop\house-price-prediction\data\Housing.csv")
 
 df= pd.get_dummies(df, drop_first=True)
+
+
 X= df.drop(columns=["price"])
 y = df["price"]
 
-
+# splitting data
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-model = LinearRegression()
 
+param_grid = {
+    'n_estimators': [100, 200, 300],
+    'max_depth': [None, 10, 20],
+    'min_samples_split': [2, 5, 10]
+}
 
-model.fit(X_train, y_train)
+grid_search = GridSearchCV(RandomForestRegressor(random_state=42), param_grid, cv=5, scoring="neg_mean_absolute_error")
+grid_search.fit(X_train, y_train)
 
+# Best params
+print("Best Parameters:", grid_search.best_params_)
 
-y_pred = model.predict(X_test)
+# Use best model
+best_model = grid_search.best_estimator_
+y_pred = best_model.predict(X_test)
+
+# Evaluate
 mae = mean_absolute_error(y_test, y_pred)
 mse = mean_squared_error(y_test, y_pred)
 rmse = np.sqrt(mse)
@@ -29,4 +50,3 @@ rmse = np.sqrt(mse)
 print(f"MAE: {mae:.2f}")
 print(f"MSE: {mse:.2f}")
 print(f"RMSE: {rmse:.2f}")
-
